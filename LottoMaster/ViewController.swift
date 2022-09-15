66de23e76ee7dfe2b3327d57a6ba19f9306bcf6c
minseong <msb0987@naver.com>
2022-09-15 17:49:28 +0900

    Initial Commit

---
+++ LottoMaster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoMaster
+//
+//  Created by 백민성 on 2022/09/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
